562180a5532b612290ad059cf6bbc347a99edbae
Evan <devandanger@gmail.com>
2015-10-09 03:33:49 -0400

    Built and running successfully

--- src/main/scala/com/mightystrong/machinelearning/App.scala
+++ src/main/scala/com/mightystrong/machinelearning/App.scala
@@ -1,15 +1,41 @@
 package com.mightystrong.machinelearning
 
+import scala.io.Source
+import java.io.File
 /**
  * @author ${user.name}
  */
-object App {
+object KNNExample {
   
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
-  }
+  	var getCurrentDirectory = new java.io.File( "." ).getCanonicalPath
+  	println(getCurrentDirectory)
+	val basePath = "KNN_Example_1.csv"
+	val testData = getDataFromCSV(new File(basePath))
+	}
 
+  def getDataFromCSV(file: File): (Array[Array[Double]], Array[Int]) = {
+	var source = scala.io.Source.fromFile(file)
+	var data = source
+		.getLines()
+		.drop(1)
+		.map(x => getDataFromString(x))
+		.toArray
+	source.close()
+	var dataPoints = data.map(x => x._1)
+	var classifierArray = data.map(x => x._2)
+	return (dataPoints, classifierArray)
+	}
+
+  def getDataFromString(dataString: String): (Array[Double], Int) = {
+	var dataArray: Array[String] = dataString.split(',')
+
+	var xCoordinate: Double = dataArray(0).toDouble
+	var yCoordinate: Double = dataArray(1).toDouble
+	var classifier: Int = dataArray(2).toInt
+
+	return (Array(xCoordinate, yCoordinate), classifier)
+	}
 }
